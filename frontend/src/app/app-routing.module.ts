f13b58374de52b47ad5b38c612a677b8a8522816
Jinhan Gao <jinhan_gao@outlook.com>
2022-08-16 14:54:23 +0300

    Creat the basic UI of journey and station show page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './pages/homepage/homepage.component';
+import { JourneyComponent } from './pages/journey/journey.component';
+import { StationComponent } from './pages/station/station.component';
 
 const routes: Routes = [
-  { path: '', component: HomepageComponent}
+  { path: '', component: HomepageComponent},
+  { path: 'journey', component: JourneyComponent},
+  { path: 'station', component: StationComponent}
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
+import { JourneyComponent } from './pages/journey/journey.component';
+import { StationComponent } from './pages/station/station.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    JourneyComponent,
+    StationComponent
   ],
   imports: [
     BrowserModule,
